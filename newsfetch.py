bcd35351d0a0a1a02f9debc8b3e7281942b2e794
Shorya-agarwal <91627681+Shorya-agarwal@users.noreply.github.com>
2023-09-29 21:18:22 +0530

    Newsfetch complete

--- newsfetch.py
+++ newsfetch.py
@@ -1,34 +1,34 @@
 from newsapi import NewsApiClient
 
-# Initialize the News API client
+# Initialize the News API client with your API key
 newsapi = NewsApiClient(api_key="99d70feeab2d40b1826e49db65fb1ef4")  # Replace with your actual API key
 
-keywords = 'weather OR air quality'
+def fetch_news(num_articles=2):
+    keywords = 'weather OR air quality'
+    """
+    Fetches news articles based on provided keywords.
 
+    Args:
+        num_articles (int): Number of articles to fetch (default is 2).
 
-
-
-def fetchnews():
+    Returns:
+        list: List of tuples containing (title, url) for each news article.
+    """
     # Fetch news articles based on the keywords
     articles = newsapi.get_everything(q=keywords)
-    # print(articles['articles'][0])
-    matrix = []
-    news1 = articles['articles'][0]
-    news1title = news1['title']
-    news1url = news1['url']
-    matrix.append((news1title,news1url))
-    news2 = articles['articles'][1]
-    news2title = news2['title']
-    news2url = news2['url']
-    matrix.append((news2title,news2url))
-    print(matrix)
-    # Print the titles and descriptions of the articles
-    # for article in articles['articles']:
-    #     print('Title:', article['title'])
-    #     print('Description:', article['description'])
-    #     print('Source:', article['source']['name'])
-    #     print('URL:', article['url'])
-    #     print('Published At:', article['publishedAt'])
-    #     print('---')
 
-fetchnews()
+    # Extract titles and URLs for the specified number of articles
+    news_list = []
+    for article in articles['articles'][:num_articles]:
+        news_list.append((article['title'], article['url']))
+
+    return news_list
+
+if __name__ == "__main__":
+    # Fetch and print the latest news
+    news_articles = fetch_news(num_articles=2)
+    for index, (title, url) in enumerate(news_articles, start=1):
+        print(f"News {index}:")
+        print(f"Title: {title}")
+        print(f"URL: {url}")
+        print("---")
